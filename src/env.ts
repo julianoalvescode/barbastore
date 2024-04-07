779987b4187130192c7c371207956403e7376cc9
Juliano Alves <julianoalves@me>
2024-04-07 01:14:00 -0300

    feat: 🎸 add opengraph-image

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,16 @@
-import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const env = createEnv({
-  client: {
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  },
-  runtimeEnv: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  },
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  APP_URL: z.string().url(),
 });
+
+const parsedEnv = envSchema.safeParse(process.env);
+if (!parsedEnv.success) {
+  console.error(
+    "Invalid environment variables",
+    parsedEnv.error.flatten().fieldErrors
+  );
+  throw new Error("Invalid environment variables.");
+}
+export const env = parsedEnv.data;
